792133714c658f1b3709623038ab8f41f6c00925
Saran Bodduluri <73103188+SaranBodduluri@users.noreply.github.com>
2023-09-18 05:01:37 +0530

    Create Pairs_differ_by_one_digit.py

---
+++ Python/Arrays/Pairs_differ_by_one_digit.py
@@ -0,0 +1,8 @@
+## given a list of integers find the number of pairs which differ by just one digit
+## brute force: double for loop and check
+## better: while loop and TWO pointers!!!
+## best: hash map
+
+def soln(numbers):
+  // numbers = [1,151,241,1,9,22,351]
+  actualCount = 0
